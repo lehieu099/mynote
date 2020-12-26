57b297cf044a72c6eb7e5c582ba4dd2dcdc97a96
lehieu099 <duyhieu099a2@gmail.com>
2020-12-26 15:54:34 +0700

    26/12

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,5 @@
-// import 'package:flutter/material.dart';
-// import 'package:mynote/ui/views/note/note_view.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: NoteView(),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
-import 'package:mynote/ui/views/note/home_screen.dart' show HomeScreens;
+import 'package:mynote/ui/views/note/note_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,13 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomeScreens(),
-      debugShowCheckedModeBanner: false,
+      home: NoteView(),
     );
   }
 }
